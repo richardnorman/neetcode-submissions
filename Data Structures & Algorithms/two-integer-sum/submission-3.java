44a82adcc4118d79e766abdd15ae159d54befca9
Richard Norman <richard.norman.id@gmail.com>
2026-08-29 18:36:45 -0500

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // return int[] with 2 indices i and j in nums so that nums[i] + nums[j] == target and i != j
+        // assuming that every input has exactly  one i and j that satify condition equality
+        // return smallest index first
+
+        // k: diff
+        // v: index
+        HashMap <Integer, Integer> indexTargetSumMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            // get the difference, the needed pre-existing entry in the map, if present would satisfy the problem statement
+            int diff = target - nums[i];
+
+            // check if the mapping already has the diff present
+            if (indexTargetSumMap.containsKey(diff)) {
+                // return the pre-existing diff's index along with the current index of the int as the previous index is ALWAYS smaller than the current int
+                return new int[] {indexTargetSumMap.get(diff), i};
+            }
+            indexTargetSumMap.putIfAbsent(nums[i], i);
+        }
+
+        return new int[0];
+    }
+}
