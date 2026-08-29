5060a694214c57d31c565625a419caa58d125704
Richard Norman <richard.norman.id@gmail.com>
2026-08-29 18:21:31 -0500

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // return int[] with 2 indices i and j in nums so that nums[i] + nums[j] == target and i != j
+        // assuming that every input has exactly  one i and j that satify condition equality
+        // return smallest index first
+
+        HashMap <Integer, Integer> indexTargetSumMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            indexTargetSumMap.putIfAbsent(nums[i], i);
+
+            // check map to see if value + another existing value is now present in the HashMap
+            // also check and see that current index being evaluated is NOT stored index
+            if (indexTargetSumMap.containsKey(target - nums[i]) && i != indexTargetSumMap.get(target - nums[i])) {
+                int[] ijIndex = new int[] {indexTargetSumMap.get(target - nums[i]), i};
+                Arrays.sort(ijIndex);
+                return ijIndex;
+            }
+        }
+
+        return new int[0];
+    }
+}
